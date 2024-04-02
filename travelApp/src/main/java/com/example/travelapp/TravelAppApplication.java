9fdf40f37078b074a3a109b886e7b770f5fd2244
luka-danger <lukathevegan@gmail.com>
2024-04-02 10:44:24 -0600

    Rebuild Project

--- travelApp/src/main/java/com/example/travelapp/TravelAppApplication.java
+++ travelApp/src/main/java/com/example/travelapp/TravelAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TravelAppApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TravelAppApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAppApplication.class, args);
+	}
 
 }

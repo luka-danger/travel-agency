fe460f17afcac5df031988187cbac4b09598c9fc
luka-danger <lukathevegan@gmail.com>
2024-04-04 15:46:28 -0600

    Part H
    Create Checkout Controller in Controllers Package with Post Mapping to place orders

---
+++ travelApp/src/main/java/com/example/travelapp/controllers/CheckoutController.java
@@ -0,0 +1,26 @@
+package com.example.travelapp.controllers;
+
+import com.example.travelapp.services.CheckoutService;
+import com.example.travelapp.services.Purchase;
+import com.example.travelapp.services.PurchaseResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping ("/api/checkout")
+public class CheckoutController {
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
+    }
+}

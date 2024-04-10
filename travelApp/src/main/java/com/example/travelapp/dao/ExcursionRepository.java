7d1ac02dc381290f00992b9f4654cc4e07c74137
luka-danger <lukathevegan@gmail.com>
2024-04-10 12:14:21 -0600

    Add Cross Origin Support

--- travelApp/src/main/java/com/example/travelapp/dao/ExcursionRepository.java
+++ travelApp/src/main/java/com/example/travelapp/dao/ExcursionRepository.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
-@CrossOrigin
+@CrossOrigin("http://localhost:4200")
 public interface ExcursionRepository extends JpaRepository<Excursion, Long> {
 }
--- travelApp/src/main/java/com/example/travelapp/dao/VacationRepository.java
+++ travelApp/src/main/java/com/example/travelapp/dao/VacationRepository.java
@@ -4,6 +4,6 @@ import com.example.travelapp.entities.Vacation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin
+@CrossOrigin("http://localhost:4200")
 public interface VacationRepository extends JpaRepository<Vacation, Long> {
 }
